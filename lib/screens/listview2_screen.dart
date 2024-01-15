c421d5459d790700e762592bc64ce3caa1aae987
fernando <rojofernando0424@gmail.com>
2024-01-15 14:06:00 -0500

    feat:Added listview1 and listview2 component using widget with lazy loading

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const ['Megaman', 'Metal Gear','Seper Smash','Final Fantasy'];
+   
+  const Listview2Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Center(child:  Text('Listview Tipo 2',style: TextStyle(color: Colors.white))),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
+      body:  ListView.separated(
+        itemCount: options.length,
+        itemBuilder: (context, index)=> ListTile(
+          title:Text(options[index]),
+          trailing: const Icon(Icons.arrow_forward_ios_outlined,color: Colors.indigo,),
+          onTap: () {
+            
+          },
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+      ),
+    );
+  }
+}
